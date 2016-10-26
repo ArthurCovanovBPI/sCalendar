21a043e4bd4b690bc7ce317271de8add37baf984
Yhtill <arthur.covanov@bpi.fr>
2016-10-26 16:36:07 +0200

    Calendar checkbuttons

--- calendar.php
+++ calendar.php
@@ -221,11 +221,11 @@
 	{
 		echo('<table class="monthTable" border="1" style="width:100%;">');
 
-		echo '<input type="checkbox" id="checkTout" name="checkTout" /><label for="checkTout">Tout</label>';
-		echo '<input type="checkbox" id="checkManifPublique" name="checkManifPublique" checked /><label for="checkManifPublique">Manifestation publique</label>';
-		echo '<input type="checkbox" id="checkManifInterne" name="checkManifInterne" checked /><label for="checkManifInterne">Manfestation / réunion interne</label>';
-		echo '<input type="checkbox" id="checkCalendaire" name="checkCalendaire" checked /><label for="checkCalendaire">Événement de type calendaire (vacances, jours fériés)</label>';
-		echo '<input type="checkbox" id="checkAdminRH" name="checkAdminRH" /><label for="checkAdminRH">Administratif et RH (pas d\'envoi à la presse)</label>';
+		echo '<input type="checkbox" id="checkTout" name="checkTout" /><label for="checkTout">Tout </label>';
+		echo '<input type="checkbox" id="checkManifPublique" name="checkManifPublique" checked /><label for="checkManifPublique">Manifestation publique </label>';
+		echo '<input type="checkbox" id="checkManifInterne" name="checkManifInterne" checked /><label for="checkManifInterne">Manfestation / réunion interne </label>';
+		echo '<input type="checkbox" id="checkCalendaire" name="checkCalendaire" checked /><label for="checkCalendaire">Événement de type calendaire (vacances, jours fériés) </label>';
+		echo '<input type="checkbox" id="checkAdminRH" name="checkAdminRH" /><label for="checkAdminRH">Administratif et RH (pas d\'envoi à la presse) </label>';
 		echo '<input type="checkbox" id="checkFinancier" name="checkFinancier" /><label for="checkFinancier">Financier (pas d\'envoi à la presse)</label>';
 
 
