1631b2d4930783d61c2a4e87ae963f6320937a40
Yhtill <arthur.covanov@bpi.fr>
2016-10-27 11:58:59 +0200

    Calendar checkbuttons

--- calendar.php
+++ calendar.php
@@ -23,11 +23,11 @@
 	$sqlManifTypeAddition = "";
 	if($_a == 0)
 	{
-		$sqlManifTypeAddition .= (($_0 != 0)? " OR type_manifestation_ID == 1" : "");
-		$sqlManifTypeAddition .= (($_1 != 0)? " OR type_manifestation_ID == 2" : "");
-		$sqlManifTypeAddition .= (($_2 != 0)? " OR type_manifestation_ID == 6" : "");
-		$sqlManifTypeAddition .= (($_3 != 0)? " OR type_manifestation_ID == 3 OR type_manifestation_ID == 4" : "");
-		$sqlManifTypeAddition .= (($_4 != 0)? " OR type_manifestation_ID == 5" : "");
+		$sqlManifTypeAddition .= (($_0 != 0)? " OR type_manifestation_ID = 1" : "");
+		$sqlManifTypeAddition .= (($_1 != 0)? " OR type_manifestation_ID = 2" : "");
+		$sqlManifTypeAddition .= (($_2 != 0)? " OR type_manifestation_ID = 6" : "");
+		$sqlManifTypeAddition .= (($_3 != 0)? " OR type_manifestation_ID = 3 OR type_manifestation_ID = 4" : "");
+		$sqlManifTypeAddition .= (($_4 != 0)? " OR type_manifestation_ID = 5" : "");
 
 		$sqlManifTypeAddition = substr($sqlManifTypeAddition, 4);
 
