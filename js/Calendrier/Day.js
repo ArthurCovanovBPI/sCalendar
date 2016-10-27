6afed60f013e03ed8e785439723b13fc1db7d067
Yhtill <arthur.covanov@bpi.fr>
2016-10-27 14:16:58 +0200

    Calendar checkbuttons

--- js/Calendrier/Day.js
+++ js/Calendrier/Day.js
@@ -12,8 +12,31 @@ function changeManifType()
 	var _3 = document.getElementById("checkAdminRH").checked;
 	var _4 = document.getElementById("checkFinancier").checked;
 
-	var newManifType = "" + _a + _0 + _1 + _2 + _3 + _4;
+	var newManifType = ((_0)?"1":"0") + ((_1)?"1":"0") + ((_2)?"1":"0") + ((_3)?"1":"0") + ((_4)?"1":"0");
+	if(_a)
+		newManifType = "11111";
 
-	alert(newManifType);
+key = encodeURI("calendarCheck");
+value = encodeURI(newManifType);
+
+var kvp = document.location.split('&');
+while(i--) 
+{
+x = kvp[i].split('=');
+
+if (x[0]==key)
+{
+x[1] = value;
+kvp[i] = x.join('=');
+break;
+}
+}
+
+if(i<0) {kvp[kvp.length] = [key,value].join('=');}
+
+//this will reload the page, it's likely better to store this until finished
+//document.location.search = kvp.join('&'); 
+
+	alert(kvp.join('&'));
 }
 
--- js/Calendrier/Month.js
+++ js/Calendrier/Month.js
@@ -16,6 +16,27 @@ function changeManifType()
 	if(_a)
 		newManifType = "11111";
 
-	alert(newManifType);
+key = encodeURI("calendarCheck");
+value = encodeURI(newManifType);
+
+var kvp = document.location.split('&');
+while(i--) 
+{
+x = kvp[i].split('=');
+
+if (x[0]==key)
+{
+x[1] = value;
+kvp[i] = x.join('=');
+break;
+}
+}
+
+if(i<0) {kvp[kvp.length] = [key,value].join('=');}
+
+//this will reload the page, it's likely better to store this until finished
+//document.location.search = kvp.join('&'); 
+
+	alert(kvp.join('&'));
 }
 
