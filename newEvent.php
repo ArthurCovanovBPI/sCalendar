7fb56de3d9e4f27d5938cc9fdd0d0f8d36b6e903
Yhtill <arthur.covanov@bpi.fr>
2016-10-28 15:00:28 +0200

    Manif time updates

--- newEvent.php
+++ newEvent.php
@@ -214,7 +214,7 @@
 				;
 
 				echo
-								'De: <input type="time" onkeydown="return false;" name="manifStart" min="08:00" max="21:30" value="08:00" step="1800">'.
+								'De: <input type="time" stepUp="alert();" onkeydown="return false;" name="manifStart" min="08:00" max="21:30" value="08:00" step="1800">'.
 								' à: <input type="time" onkeydown="return false;" name="manifEnd" min="08:30" max="22:00" value="09:00" step="1800">'
 				;
 
